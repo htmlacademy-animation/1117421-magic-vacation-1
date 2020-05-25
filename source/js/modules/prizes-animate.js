84c0424b50a35a8c6de064d055bae7363ac6e048
AnnaChernova <ashubina0810@mail.ru>
2020-05-25 18:15:54 +0300

    Реализовываем анимацию заголовков на экране «Результат игры»

--- source/js/modules/prizes-animate.js
+++ source/js/modules/prizes-animate.js
@@ -1,18 +1,18 @@
 const animatePrizes = (isActivePrizesScreen) => {
-  const primaryAwardWrap = document.querySelector(`.prizes__item--journeys .prizes__icon`);
-  const secondaryAwardWrap = document.querySelector(`.prizes__item--cases .prizes__icon`);
-  const additionalAwardWrap = document.querySelector(`.prizes__item--codes .prizes__icon`);
+  const primaryAwardWrap = document.querySelector(`.prizes__item--journeys .prizes__icon--desktop`);
+  const secondaryAwardWrap = document.querySelector(`.prizes__item--cases .prizes__icon--desktop`);
+  const additionalAwardWrap = document.querySelector(`.prizes__item--codes .prizes__icon--desktop`);
   const prizesItems = document.querySelectorAll(`.prizes__item`);
   if (isActivePrizesScreen && primaryAwardWrap.innerHTML === ``) {
     prizesItems.forEach((item) => {
       item.classList.add(`prizes__item--animate`);
     });
-    primaryAwardWrap.innerHTML = `<picture><source srcset="img/prize1-mob.svg?id=${Math.random()}" media="(orientation: portrait)"><img src="img/primary-award.svg?id=${Math.random()}"></picture>`;
+    primaryAwardWrap.innerHTML = `<img src="img/primary-award.svg?id=${Math.random()}">`;
     setTimeout(() => {
-      secondaryAwardWrap.innerHTML = `<picture><source srcset="img/prize2-mob.svg?id=${Math.random()}" media="(orientation: portrait)"><img src="img/secondary-award.svg?id=${Math.random()}"></picture>`;
+      secondaryAwardWrap.innerHTML = `<img src="img/secondary-award.svg?id=${Math.random()}">`;
     }, 5200);
     setTimeout(() => {
-      additionalAwardWrap.innerHTML = `<picture><source srcset="img/prize3-mob.svg?id=${Math.random()}" media="(orientation: portrait)"><img src="img/additional-award.svg?id=${Math.random()}"></picture>`;
+      additionalAwardWrap.innerHTML = `<img src="img/additional-award.svg?id=${Math.random()}">`;
     }, 8500);
   } else {
     prizesItems.forEach((item) => {
