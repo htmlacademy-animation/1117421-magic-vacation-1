cf771b0ff0b51b813bc9a401e5475cfe25b87a82
AnnaChernova <ashubina0810@mail.ru>
2021-02-02 23:09:20 +0300

    Программируем растровый эффект смещения Hue

--- source/js/helpers/3d.js
+++ source/js/helpers/3d.js
@@ -18,37 +18,3 @@ export const setup3d = (far = 100, fov = 45) => {
     camera
   };
 };
-
-export const simpleVertexShader = `
-uniform mat4 projectionMatrix;
-uniform mat4 modelMatrix;
-uniform mat4 viewMatrix;
-
-attribute vec3 position;
-attribute vec3 normal;
-attribute vec2 uv;
-
-varying vec2 vUv;
-
-void main() {
-  vUv = uv;
-
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 );
-}
-`;
-
-export const simpleFragmentShader = `
-precision mediump float;
-
-uniform sampler2D map;
-
-varying vec2 vUv;
-
-void main() {
-
-vec4 texel = texture2D( map, vUv );
-
-gl_FragColor = texel;
-
-}
-`;
